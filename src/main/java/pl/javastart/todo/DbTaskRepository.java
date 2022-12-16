99f009d59598b2b81c30577643eb52dbfb3156c0
User <user@its.me>
2022-12-16 12:17:22 +0100

    update option (task duration counting)

--- src/main/java/pl/javastart/todo/DbTaskRepository.java
+++ src/main/java/pl/javastart/todo/DbTaskRepository.java
@@ -2,28 +2,27 @@ package pl.javastart.todo;
 
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
+
 @Repository
-class SimpleTaskRepository implements TaskRepository {
+class DbTaskRepository implements TaskRepository {
     private final EntityManager entityManager;
     private static long nextId = 1;
 
-    public SimpleTaskRepository(EntityManager entityManager) {
+    public DbTaskRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-    @Transactional
+
     @Override
     public Task save(Task task) {
         task.setId(nextId);
-        nextId++;
         entityManager.persist(task);
+        nextId++;
         return task;
     }
+
     @Override
     public Optional<Task> findById(Long id) {
-        Task task = entityManager.find(Task.class, id);
-        return Optional.ofNullable(task);
+        return Optional.ofNullable(entityManager.find(Task.class, id));
     }
 }
---
+++ src/main/java/pl/javastart/todo/TaskService.java
@@ -0,0 +1,53 @@
+package pl.javastart.todo;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import pl.javastart.todo.dto.*;
+import pl.javastart.todo.exception.*;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Transactional
+    public Long saveTask(TaskDto task) {
+        Task taskToSave = new Task(task.getTitle(), task.getDescription(), task.getPriority());
+        Task savedTask = taskRepository.save(taskToSave);
+        return savedTask.getId();
+    }
+
+    public Optional<String> getTaskInfo(Long taskId) {
+        return taskRepository.findById(taskId).map(Task::toString);
+    }
+
+    @Transactional
+    public LocalDateTime startTask(Long taskId) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(TaskNotFoundException::new);
+        if (task.getStartTime() != null) {
+            throw new TaskAlreadyStartedException();
+        }
+        task.setStartTime(LocalDateTime.now());
+        return task.getStartTime();
+    }
+
+    @Transactional
+    public TaskDurationDto endTask(Long taskId) {
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(TaskNotFoundException::new);
+        if (task.getStartTime() == null) {
+            throw new TaskNotStartedException();
+        } else if (task.getCompletionTime() != null) {
+            throw new TaskAlreadyCompletedException();
+        }
+        task.setCompletionTime(LocalDateTime.now());
+        return new TaskDurationDto(task.getStartTime(), task.getCompletionTime());
+    }
+}
